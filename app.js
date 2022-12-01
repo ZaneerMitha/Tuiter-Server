00ff8f09060cd9d1811332ba5dcd313226a6fbe6
Zaneer Mitha <mithazaneer@gmail.com>
2022-11-30 21:43:14 -0500

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -14,5 +14,9 @@ UserController(app)
 TuitsController(app);
 app.listen(process.env.PORT || 4000);
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 
 
